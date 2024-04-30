83f8e00e9f90576fe7116920be757eda1e3dd144
enricosotelo <enricosotelo45@gmail.com>
2024-04-30 15:00:01 -0300

    aboutme

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,44 @@
 <template>
+<main>
+  <div class="background">
+    
+  <div class="container-text">
+    <div class="text">
+      <h1>Sobre mim</h1>
+      <img src="/public/perfil.jpeg" class="img-thumbnail" alt="...">
+        <h1>Enrico Sotelo</h1>
+        <p>19 anos, nascido em Santos e em marília há 9 anos.</p>
+        <p> Atualmente, estou no terceiro período do curso de Análise e Desenvolvimento de Sistemas, explorando minha paixão pela tecnologia e pela resolução de problemas.
+          Além dos estudos, estou envolvido em uma experiência prática como aprendiz na Nestlé. Embora meu papel atual no almoxarifado técnico não esteja diretamente alinhado com minha área de estudo, vejo essa oportunidade como uma chance de adquirir habilidades valiosas e ampliar meu conhecimento em um ambiente corporativo.
+          Estou comprometido em aproveitar ao máximo cada experiência, seja ela acadêmica ou profissional. Cada desafio que enfrento contribui para meu crescimento pessoal e profissional, preparando-me para os futuros caminhos que desejo explorar.</p>
+    </div>
+</div>
+</div>
 
+
+  
+
+
+
+
+</main>
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins" , sans-serif;
+  text-decoration: none;
+  list-style: none;
+}
+p{
+  padding: 10px;
+
+}
+
+
+
+
 </style>
